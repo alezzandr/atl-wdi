2a014179bf26d3d47431dbbffbabf78b2a41520e
alezzander <alezzan@gmail.com>
2017-09-21 22:03:05 -0400

    controllers/pirates.js

--- homework/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
+++ homework/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
@@ -1,1 +1,31 @@
 //your code below
+var express = require("express");
+var router = express.Router();
+var pirates = require('../models/pirates.js');
+
+//for root pirate page
+router.get('/', function(req, res){
+	router.post("pirates/index.hbs", {
+		pirates: pirates
+	});
+});
+
+
+router.get('/new', function(req, res){
+	router.post("pirates/new.hbs");
+});
+
+
+//this is for each pirate page
+router.get('/:id', function(req, res){
+
+	//grab the pirate by id
+	var showPirate = pirates[req.params.id];
+
+	router.post("pirates/show.hbs", {
+		pirate: showPirate
+	});
+});
+
+//exports
+module.exports = router;
--- homework/express/express-pirates-create-read/pirates-wiki/server.js
+++ homework/express/express-pirates-create-read/pirates-wiki/server.js
@@ -1,1 +1,23 @@
 //your code here
+var express = require("express");
+var app = express();
+var logger = require("morgan");
+var bodyParser = require("body-parser");
+var hbs = require('hbs');
+
+
+app.use(logger("dev"));
+//these are for bodyParser
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+//set handlebars as view engine
+app.set("view engine", "hbs");
+app.set('views', './views');
+
+//controllers for `/pirates` resource
+var pirateController = require('./controllers/pirates.js');
+app.use("/pirates", pirateController);
+
+app.listen(3000, function(req, res){
+	console.log("listening");
+});
